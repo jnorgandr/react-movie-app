7d39465586f71e91303219ff4442482f76c18d4b
studiominti <studiominti@gmail.com>
2020-10-04 04:00:46 -0300

    chore(ui): styling improvements and React router links

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## React movie app exercise
 
-Movie list exercise with react, consuming API REST from YTS with axios.
+Movie list exercise with React, consuming API REST from YTS with axios and some routing with React Router
+
+# You can visit the page here:
+
+https://studiominti.github.io/react-movie-app/
---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About this page:</h1>
+      <p>
+        Movie list exercise with React, consuming API REST from YTS with axios
+        and some routing with React Router
+      </p>
+    </div>
+  );
+}
+
+export default About;
