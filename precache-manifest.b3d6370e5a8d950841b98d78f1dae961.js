3b7b14e034769a39e52e92b868194a120a85bd95
studiominti <studiominti@gmail.com>
2020-10-04 02:07:12 -0300

    Updates

---
+++ precache-manifest.b3d6370e5a8d950841b98d78f1dae961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a713e123870c61e84871d69ea5345754",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "f6e3c17069914929ccfa",
+    "url": "/react-movie-app/static/css/main.defba746.chunk.css"
+  },
+  {
+    "revision": "9f80a518c59343a92732",
+    "url": "/react-movie-app/static/js/2.6e20bb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.6e20bb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e3c17069914929ccfa",
+    "url": "/react-movie-app/static/js/main.edfaddfa.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
